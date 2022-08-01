d3b8973dca1228a7bfebd5abb1cf6a8d508c2662
Siarhei Yafimenka <h1_n1@mail.ru>
2022-08-01 19:01:04 +0300

    feat: add task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+   const Open_Brackets = [];
+   const Brackets_Pair = {};
+
+   for (let i = 0; i < bracketsConfig.length; i++) {
+      let key = bracketsConfig[i][1];
+      let value = bracketsConfig[i][0];
+      Open_Brackets.push(value);
+      Brackets_Pair[key] = value;
+   }
+
+   let stack = [];
+   for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+      let topElement = stack[stack.length - 1];
+      if (Brackets_Pair[currentSymbol] === topElement && stack.length > 0) {
+         stack.pop();
+      }
+      else {
+         if (Open_Brackets.includes(currentSymbol)) stack.push(currentSymbol);
+         if (stack.length === 0) return false;
+      }
+   }
+   return stack.length === 0;
 }
